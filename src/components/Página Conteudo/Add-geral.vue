c21f5f61e27e6c7d8b2f39a37a0f074cbae533a8
Helena Della Giustina <haguiar@imply.com>
2022-12-29 13:27:14 -0300

    ajuste

--- src/components/Página Conteudo/Add-geral.vue
+++ src/components/Página Conteudo/Add-geral.vue
@@ -2,8 +2,8 @@
     <section>
         <div class="grid grid-cols-7 gap-4 place-items-center">
             <h1 style="color: var(--texto-segundo);" class="">Nome</h1>
-            <input style="background-color: var(--cinza-input);" type="text" class="input input-bordered input-xs col-span-3 w-64">
-            <p style="color: var(--texto-segundo);" class="col-span-3 text-sm">Nome qie será exibido em sua lista de conteúdos e como link em sua página!</p>
+            <input type="text" class="input input-bordered input-xs col-span-3 w-64 input-back">
+            <p style="color: var(--texto-segundo);" class="col-span-3 text-sm">Nome que será exibido em sua lista de conteúdos e como link em sua página!</p>
         </div>
     </section>
 </template>
@@ -21,5 +21,8 @@
 </script>
 
 <style scoped>
-
+    .input-back{
+        background-color: var(--cinza-input);
+        color: var(--texto-segundo);
+    }
 </style>
